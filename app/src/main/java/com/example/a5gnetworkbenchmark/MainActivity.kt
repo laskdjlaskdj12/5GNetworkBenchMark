cd7ff06cc81626566e5a58818eae3201f7dd678e
InHoeKu <laskdjlaskdj12@gmail.com>
2019-09-07 12:38:21 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a5gnetworkbenchmark/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.a5gnetworkbenchmark
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        activeButton.setOnClickListener{
+            //스피드테스트를 시작함
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a5gnetworkbenchmark/service/NetworkBenchmarkService.kt
@@ -0,0 +1,12 @@
+package com.example.a5gnetworkbenchmark.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class NetworkBenchmarkService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
